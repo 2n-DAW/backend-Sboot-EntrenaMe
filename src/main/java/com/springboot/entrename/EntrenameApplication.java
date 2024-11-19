340a00b121350bd04beeefb72c5c820763141a87
fmontesdev <f.montesdoria@gmail.com>
2024-11-19 05:48:47 +0100

    Inicio de proyecto

---
+++ src/main/java/com/springboot/entrename/EntrenameApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.entrename;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrenameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/entrename/EntrenameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.entrename;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
