882fe7cc1e8675abe9d730ce3b6ce8c82523a07f
fmontesdev <f.montesdoria@gmail.com>
2024-11-28 22:42:56 +0100

    Añadida dependencia filtros dinámicos: specification-arg-resolver

--- src/main/java/com/springboot/entrename/EntrenameApplication.java
+++ src/main/java/com/springboot/entrename/EntrenameApplication.java
@@ -2,8 +2,10 @@ package com.springboot.entrename;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+// @EnableJpaRepositories("com.springboot.entrename")
 public class EntrenameApplication {
 
 	public static void main(String[] args) {
