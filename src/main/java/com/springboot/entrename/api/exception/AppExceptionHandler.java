ca55895c909c079548f253b12d5591160dfa561a
fmontesdev <f.montesdoria@gmail.com>
2024-12-05 19:20:53 +0100

    Mejorado manejo de errores

--- src/main/java/com/springboot/entrename/api/exception/AppExceptionHandler.java
+++ src/main/java/com/springboot/entrename/api/exception/AppExceptionHandler.java
@@ -2,28 +2,50 @@ package com.springboot.entrename.api.exception;
 
 import com.springboot.entrename.domain.exception.AppException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class AppExceptionHandler {
-    @ExceptionHandler(AppException.class)
-    public ResponseEntity<ErrorResponse> handleAppException(AppException exception) {
-        ErrorResponse errorResponse = new ErrorResponse(exception.getError().getMessage());
-        return new ResponseEntity<>(errorResponse, exception.getError().getStatus());
+    // Maneja errores genéricos
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessages> handleGenericException(Exception exception) {
+        return responseErrorMessages(
+                Map.of("error", "internal server error"), // Error code, Error message
+                HttpStatus.INTERNAL_SERVER_ERROR // Error http status
+        );
     }
 
-    // Clase interna para la respuesta de error
-    private static class ErrorResponse {
-        private final String message;
+    // Maneja errores específicos
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<ErrorMessages> handleAppException(AppException exception) {
+        return responseErrorMessages(
+            Map.of(exception.getError().name(), exception.getMessage()), // Error code, Error message
+            exception.getError().getStatus() // Error http status
+        );
+    }
 
-        public ErrorResponse(String message) {
-            this.message = message;
-        }
+    // Maneja errores de validación en los DTOs
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessages> handleValidationErrors(MethodArgumentNotValidException exception) {
+        Map<String, String> fieldError = exception.getBindingResult().getFieldErrors().stream()
+            .collect(Collectors.toMap(
+                FieldError::getField, // Error field
+                FieldError::getDefaultMessage // Error message
+            ));
+        return responseErrorMessages(fieldError, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 
-        public String getMessage() {
-            return message;
-        }
+    private ResponseEntity<ErrorMessages> responseErrorMessages(Map<String, String> error, HttpStatus status) {
+        ErrorMessages errorMessages = new ErrorMessages();
+        error.forEach(errorMessages::addError);
+        return new ResponseEntity<>(errorMessages, status);
     }
 }
--- src/main/java/com/springboot/entrename/domain/exception/Error.java
+++ src/main/java/com/springboot/entrename/domain/exception/Error.java
@@ -7,17 +7,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum Error {
-    DUPLICATED_USER("El nombre de usuario o el email están duplicados", HttpStatus.UNPROCESSABLE_ENTITY),
+    DUPLICATED_USERNAME("El nombre de usuario ya está en uso. Introduce otro", HttpStatus.UNPROCESSABLE_ENTITY),
+    DUPLICATED_EMAIL("El correo electrónico ya está en uso. Introduce otro", HttpStatus.UNPROCESSABLE_ENTITY),
     LOGIN_INFO_INVALID("Los datos del login no son correctos", HttpStatus.UNPROCESSABLE_ENTITY),
-    ALREADY_FOLLOWED_USER("already followed user", HttpStatus.UNPROCESSABLE_ENTITY),
-    ALREADY_FAVORITED_ARTICLE("already followed user", HttpStatus.UNPROCESSABLE_ENTITY),
+    // ALREADY_FOLLOWED_USER("already followed user", HttpStatus.UNPROCESSABLE_ENTITY),
+    // ALREADY_FAVORITED_ARTICLE("already followed user", HttpStatus.UNPROCESSABLE_ENTITY),
 
     USER_NOT_FOUND("Usuario no encontrado", HttpStatus.NOT_FOUND),
     COURT_NOT_FOUND("Pista no encontrada", HttpStatus.NOT_FOUND),
     SPORT_NOT_FOUND("Deporte no encontrado", HttpStatus.NOT_FOUND),
     ACTIVITY_NOT_FOUND("Actividad no encontrada", HttpStatus.NOT_FOUND),
-    FOLLOW_NOT_FOUND("such follow not found", HttpStatus.NOT_FOUND),
-    FAVORITE_NOT_FOUND("favorite not found", HttpStatus.NOT_FOUND),
+    // FOLLOW_NOT_FOUND("such follow not found", HttpStatus.NOT_FOUND),
+    // FAVORITE_NOT_FOUND("favorite not found", HttpStatus.NOT_FOUND),
     ;
 
     private final String message;
