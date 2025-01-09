62786db0134d5f695c38ee2c5b0a95daaa5a68cf
fmontesdev <f.montesdoria@gmail.com>
2025-01-09 16:21:55 +0100

    Ajustado dotenv

--- src/main/java/com/springboot/entrename/infra/config/DotenvConfig.java
+++ src/main/java/com/springboot/entrename/infra/config/DotenvConfig.java
@@ -4,30 +4,30 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import jakarta.annotation.PostConstruct;
+// import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class DotenvConfig {
-    private final Dotenv dotenv = Dotenv.load();
+    // private final Dotenv dotenv = Dotenv.load();
 
-    // Carga las variables de entorno del archivo .env en las propiedades del sistema
-    @PostConstruct
-    public void init() {
-        dotenv.entries().forEach(entry -> {
-            // System.out.println("Cargando variable: " + entry.getKey() + " = " + entry.getValue());
-            if (System.getProperty(entry.getKey()) == null) {
-                System.setProperty(entry.getKey(), entry.getValue());
-            }
-        });
-    }
-
-    @Bean
-    public Dotenv getDotenv() {
-        return dotenv;
-    }
+    // // Carga las variables de entorno del archivo .env en las propiedades del sistema
+    // @PostConstruct
+    // public void init() {
+    //     dotenv.entries().forEach(entry -> {
+    //         // System.out.println("Cargando variable: " + entry.getKey() + " = " + entry.getValue());
+    //         if (System.getProperty(entry.getKey()) == null) {
+    //             System.setProperty(entry.getKey(), entry.getValue());
+    //         }
+    //     });
+    // }
 
     // @Bean
-    // public Dotenv dotenv() {
-    //     return Dotenv.load(); // Esto carga automáticamente el archivo .env
+    // public Dotenv getDotenv() {
+    //     return dotenv;
     // }
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.load(); // Esto carga automáticamente el archivo .env
+    }
 }
